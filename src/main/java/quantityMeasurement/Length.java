47ffe51e5dff5e1d79b15cce745100cd1cf888b3
Sheralram <ramkrishna.sheral@gmail.com>
2021-09-23 16:38:56 +0530

    Added : Compare Volume in Litre and Gallon

--- src/main/java/quantityMeasurement/Length.java
+++ src/main/java/quantityMeasurement/Length.java
@@ -6,20 +6,10 @@ public class Length {
     private static final double FEET_TO_INCH = 12.0;
     private static final double FEET_TO_YARD = 3.0;
     private static final double INCH_TO_CENTIMETER = 2.54 ;
+    private static final double GALLON_TO_LITRES = 3.78;
+    private static final double LITRES_TO_MILLIMETER = 1000;
 
-    public double addLengthsToInches(Length value) {
-        if (this.unit.equals(Unit.INCH)&&(value.unit.equals(Unit.INCH)))
-            return this.value + value.value;
-        if(this.unit.equals(Unit.FEET) && value.unit.equals(Unit.INCH))
-            return (this.value * FEET_TO_INCH) + value.value;
-        if(this.unit.equals(Unit.FEET) && value.unit.equals(Unit.FEET))
-            return (this.value * FEET_TO_INCH) + (value.value * FEET_TO_INCH);
-        if(this.unit.equals(Unit.INCH) && value.unit.equals(Unit.CENTIMETER))
-            return (this.value) + (value.value / INCH_TO_CENTIMETER);
-    return 0;
-    }
-
-    enum Unit { FEET, INCH, YARD, CENTIMETER};
+    enum Unit { FEET, INCH, YARD, CENTIMETER,GALLON, LITRES, MILLIMETER};
 
     private final Unit unit;
     private final double value;
@@ -38,7 +28,23 @@ public class Length {
             return Double.compare(this.value, that.value*FEET_TO_YARD ) == 0;
         if(this.unit.equals(Unit.INCH) && that.unit.equals(Unit.CENTIMETER))
             return Double.compare(this.value*INCH_TO_CENTIMETER , that.value) == 0;
+        if (this.unit.equals(Unit.GALLON) && that.unit.equals(Unit.LITRES))
+            return Double.compare(this.value * GALLON_TO_LITRES, that.value) == 0;
+        if (this.unit.equals(Unit.LITRES) && that.unit.equals(Unit.MILLIMETER))
+            return Double.compare(this.value * LITRES_TO_MILLIMETER, that.value) == 0;
         return false;
+    }
+
+    public double addLengthsToInches(Length value) {
+        if (this.unit.equals(Unit.INCH)&&(value.unit.equals(Unit.INCH)))
+            return this.value + value.value;
+        if(this.unit.equals(Unit.FEET) && value.unit.equals(Unit.INCH))
+            return (this.value * FEET_TO_INCH) + value.value;
+        if(this.unit.equals(Unit.FEET) && value.unit.equals(Unit.FEET))
+            return (this.value * FEET_TO_INCH) + (value.value * FEET_TO_INCH);
+        if(this.unit.equals(Unit.INCH) && value.unit.equals(Unit.CENTIMETER))
+            return (this.value) + (value.value / INCH_TO_CENTIMETER);
+        return 0;
     }
 
     @Override
