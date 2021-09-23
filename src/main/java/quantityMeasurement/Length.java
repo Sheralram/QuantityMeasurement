199053608730d63511319ad0817c06c130a62ede
Sheralram <ramkrishna.sheral@gmail.com>
2021-09-23 16:49:29 +0530

    Added : Volume in Litres

--- src/main/java/quantityMeasurement/Length.java
+++ src/main/java/quantityMeasurement/Length.java
@@ -7,9 +7,9 @@ public class Length {
     private static final double FEET_TO_YARD = 3.0;
     private static final double INCH_TO_CENTIMETER = 2.54 ;
     private static final double GALLON_TO_LITRES = 3.78;
-    private static final double LITRES_TO_MILLIMETER = 1000;
+    private static final double LITRES_TO_MILLILITRE = 1000;
 
-    enum Unit { FEET, INCH, YARD, CENTIMETER,GALLON, LITRES, MILLIMETER};
+    enum Unit { FEET, INCH, YARD, CENTIMETER,GALLON, LITRES, MILLILITRE};
 
     private final Unit unit;
     private final double value;
@@ -30,8 +30,8 @@ public class Length {
             return Double.compare(this.value*INCH_TO_CENTIMETER , that.value) == 0;
         if (this.unit.equals(Unit.GALLON) && that.unit.equals(Unit.LITRES))
             return Double.compare(this.value * GALLON_TO_LITRES, that.value) == 0;
-        if (this.unit.equals(Unit.LITRES) && that.unit.equals(Unit.MILLIMETER))
-            return Double.compare(this.value * LITRES_TO_MILLIMETER, that.value) == 0;
+        if (this.unit.equals(Unit.LITRES) && that.unit.equals(Unit.MILLILITRE))
+            return Double.compare(this.value * LITRES_TO_MILLILITRE, that.value) == 0;
         return false;
     }
 
@@ -44,6 +44,13 @@ public class Length {
             return (this.value * FEET_TO_INCH) + (value.value * FEET_TO_INCH);
         if(this.unit.equals(Unit.INCH) && value.unit.equals(Unit.CENTIMETER))
             return (this.value) + (value.value / INCH_TO_CENTIMETER);
+        return 0;
+    }
+    public double addVolumesToLitres(Length litres) {
+        if(this.unit.equals(Unit.GALLON) && litres.unit.equals(Unit.LITRES))
+            return (this.value * GALLON_TO_LITRES) + litres.value;
+        if(this.unit.equals(Unit.LITRES) && litres.unit.equals(Unit.MILLILITRE))
+            return (this.value * LITRES_TO_MILLILITRE) + litres.value;
         return 0;
     }
 
